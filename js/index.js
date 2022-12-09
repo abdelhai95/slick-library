89cae30fb4c058227654b5922c0105d97f452a9f
abdelhai95 <abdelhaijamal95@gamil.com>
2022-12-09 16:18:49 +0200

    final version fading setup

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".slick").slick({
+    arrows: true,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: "linear",
+  });
+});
